2d938765b472996c67042fc6a82f952b63257cda
VINICIUS BRITO <2038456@fmu.interno>
2024-03-27 19:49:48 -0300

    Inicial da aula

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PGM2024"
+include(":app")
+ 
