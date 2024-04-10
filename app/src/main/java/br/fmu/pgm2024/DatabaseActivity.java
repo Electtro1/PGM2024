b23e68d02bd3c505c0eac1f0319028147230258d
viniciusbrito97@yahoo.com.br <Electtro97@>
2024-04-10 19:37:48 -0300

    Projeto Gorjeta em Java + Android Studio

---
+++ app/src/main/java/br/fmu/pgm2024/DatabaseActivity.java
@@ -0,0 +1,14 @@
+package br.fmu.pgm2024;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DatabaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+    }
+}
