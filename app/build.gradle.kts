09126c547b744978f39d87dd3f558972e4527221
viniciusbrito97@yahoo.com.br <Electtro97@>
2024-04-10 20:57:18 -0300

    Projeto Gorjeta em Java + Android Studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     implementation("androidx.room:room-common:2.6.1")
     implementation("com.android.volley:volley:1.2.1")
     annotationProcessor("androidx.room:room-compiler:2.6.1")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/br/fmu/pgm2024/DatabaseActivity.java
+++ app/src/main/java/br/fmu/pgm2024/DatabaseActivity.java
@@ -3,12 +3,75 @@ package br.fmu.pgm2024;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DatabaseActivity extends AppCompatActivity {
+
+    private EditText editTextRe;
+    private EditText editTextNome;
+    private EditText editTextDataAdmissao;
+    private EditText editTextSalario;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+        editTextRe = findViewById(R.id.editTextRE);
+        editTextNome = findViewById(R.id.editTextNome);
+        editTextDataAdmissao = findViewById(R.id.editTextDataAdmissao);
+        editTextSalario = findViewById(R.id.editTextSalario);
     }
+
+    private void Limpar(){
+        editTextRe.getText().clear();
+        editTextNome.getText().clear();
+        editTextDataAdmissao.getText().clear();
+        editTextSalario.getText().clear();
+
+    }
+
+    public void cadastrar (View view){
+
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        String nome = editTextNome.getText().toString();
+        Date dataAdmissao;
+        try {
+            dataAdmissao = dateFormat.parse(editTextDataAdmissao.getText().toString());
+        } catch (ParseException e){
+            dataAdmissao = new Date();
+        }
+        double salario = Double.parseDouble(editTextSalario.getText().toString());
+        Funcionario f = new Funcionario(re,nome,dataAdmissao,salario);
+
+        if (view.getId() == R.id.buttonCadastrar){
+            dao.insert(f);
+        } else if (view.getId() == R.id.buttonExcluir) {
+            dao.delete(f);
+        } else if (view.getId() == R.id.buttonAtualizar) {
+            dao.update(f);
+        }
+
+        dao.insert(f);
+        Limpar();
+    }
+
+    public void buscar (View view){
+        AppDatabase db = AppDatabase.getInstance(this);
+        FuncionarioDao dao = db.funcionarioDao();
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        Funcionario f = dao.buscarPeloRe(re);
+        editTextNome.setText(f.getNome());
+        editTextDataAdmissao.setText(dateFormat.format(f.getDataAdmissao()));
+        editTextSalario.setText(Double.toString(f.getSalario()));
+
+    }
+
 }
